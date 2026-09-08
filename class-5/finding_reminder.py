785e1a9f53647336d3dbc8bbb744e1597188dd05
abhanukaushik-ctrlq <a.bhanukaushik@gmail.com>
2026-09-08 15:06:39 +0530

    notes.

--- class-5/finding_reminder.py
+++ class-5/finding_reminder.py
@@ -3,6 +3,7 @@ y = 19                          # this way is usefull when you can"t use "%"
 ans = x - int(x/y)*y
 print(ans)
 # this is divident = divisor*quotent + reminder
+#this will not work if the number is negative
 
 #this is genrally used
 a = x%y
