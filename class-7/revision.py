9391c5cc2d49d19eb63f9ace4469c010e810ca32
abhanukaushik-ctrlq <a.bhanukaushik@gmail.com>
2026-09-09 15:52:27 +0530

    notes

--- class-7/revision.py
+++ class-7/revision.py
@@ -1,3 +1,9 @@
 #remember that in "/" operatior the result is always a float and it tends to approach 0
 # in "//" operator the result is always an integer and it tends to approach -infinity
 
+q = -7%4
+print(q)
+# ans is 1 because -7 = -2*4 + 1
+for i in range(1,1000000000000000):
+    print(2**i)
+
